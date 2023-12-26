7aa91873f2c26e1a27a415e4135e6a04b00b5372
algavhane <ashishgavhane509@gmail.com>
2023-12-26 15:09:04 +0530

    01_variableAssign.js
    01_variableAssign.js

---
+++ 01_variableAssign.js
@@ -0,0 +1,27 @@
+var myFullName;
+console.log("First Name :" ,myFullName);
+
+myFullName = "Ashish";
+console.log("First Name :", myFullName);
+
+myFullName = "Ashish Laxman Gavhane";
+console.log("Complete Name :" ,myFullName);
+
+var myLove = "I love only JS";
+console.log(myLove);
+
+var FirstName = "Ashish";
+var lastName = "Gavhane";
+var age = "22";
+var address = "Sangola";
+var pinCode = "413307";
+var gender = "Male";
+var isMarried = "No";
+
+console.log("First Name : ", FirstName);
+console.log("last Name : " , lastName );
+console.log("Age :" , age);
+console.log("Address :" , address);
+console.log("Pin Code :" , pinCode);
+console.log("Gender :" , gender);
+console.log("is Married :" , isMarried); 
---
+++ helloWord.js
@@ -0,0 +1,14 @@
+console.log("Hello Js , I am Ashish");
+var myName; // Varibale Declartion
+
+console.log(myName); // undefined
+ 
+myName = "Ashish"; // Variable Intialization
+console.log("First Name: ",myName);
+
+var age = "32"; // Varibale declaration and Initialization
+console.log("Age:", age);
+
+myName = "Ashish Gavhane"; // Varibale update
+console.log("Full name: ", myName);
+
