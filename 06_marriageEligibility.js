7aa169cc4f263aae2a91a6f1f277632832af2f73
algavhane <ashishgavhane509@gmail.com>
2024-01-05 00:29:18 +0530

    06_marriageEligibility.js
    06_marriageEligibility.js

---
+++ 06_marriageEligibility.js
@@ -0,0 +1,24 @@
+
+
+
+var marriageEligibility = function (gender, age) {
+  if (gender == "Male" && age >= 21) {
+    console.log(
+      `Gender is ${gender} & Age is ${age}, then you are eligible for marriage.`
+    );
+  } else if (gender == "Female" && age >= 18) {
+    console.log(
+      `Gender is ${gender} & Age is ${age}, then you are eligible for marriage.`
+    );
+  } else {
+    console.log(
+     `Gender is ${gender} & Age is ${age}, then you are not eligible for marriage.`
+    );
+  }
+};
+marriageEligibility("Male", 17);
+marriageEligibility("Male", 25);
+marriageEligibility("Female", 28);
+marriageEligibility("Female", 16);
+marriageEligibility("Other", 35);
+marriageEligibility("Other", 41);
