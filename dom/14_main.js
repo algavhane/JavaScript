0d9f4650d731d31a91eb75cbef578463d123a525
algavhane <ashishgavhane509@gmail.com>
2024-01-29 17:29:14 +0530

    14_main
    14_main

---
+++ dom/14_main.js
@@ -0,0 +1,10 @@
+
+
+const color = document.querySelector(`h2`);
+color.style.color="red";
+
+const profileColor = document.querySelector(`#Profile`);
+profileColor.style.color="blue";
+
+const paragrap = document.querySelector('p');
+paragrap.innerHTML = 'Project Description: College Management';
